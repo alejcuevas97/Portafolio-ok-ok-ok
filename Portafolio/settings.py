02148ba972ca4ff387b058199c31148431518d77
alejcuevas97 <alejcuevas97@gmail.com>
2026-05-02 10:49:32 -0400

    db

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -4,6 +4,7 @@ import environ
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import dj_database_url
 
 env=environ.Env()
 environ.Env.read_env()
@@ -100,16 +101,14 @@ WSGI_APPLICATION = 'Portafolio.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db(
-        'DATABASE_URL',
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
+   #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+     #   'NAME': BASE_DIR / 'db.sqlite3',
+    'default':
+        dj_database_url.config('DATABASE_URL',conn_max_age=600,ssl_require=True)
+   }
+    
 }
-DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=600)
-
-# For cloud providers that require SSL for PostgreSQL connections.
-if env.bool('DATABASE_REQUIRE_SSL', default=False):
-    DATABASES['default'].setdefault('OPTIONS', {})['sslmode'] = 'require'
 
 
 
